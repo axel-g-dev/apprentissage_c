e7b7a51733b6783c3ed160c3a931e5fcb0d6d6c4
Axel <axel.gantou@carnus.fr>
2025-11-04 16:16:52 +0100

    Fin de l'exercice 2.

---
+++ Codes/Exercices/Exercices_2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+
+int main() {
+
+    double a = 5;
+    double b = 2;
+    double c = a / b;
+    std::cout << "La valeur de la division est : " << c << std::endl;
+    return 0;
+}
