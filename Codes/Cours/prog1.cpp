5b15a4c2405bdafd6a5a05a9d9c405164887eaa5
Axel <axel.gantou@carnus.fr>
2025-11-06 10:41:01 +0100

    Ajout de la fonction addition & soustraction à prog1.cpp

--- Codes/Cours/prog1.cpp
+++ Codes/Cours/prog1.cpp
@@ -2,25 +2,30 @@
 using namespace std;
 
 // Déclaration (Prototype de ma fonction)
-int addition(int a, int b); 
-int soustraction(int a, int b);   
+float addition(float a, float b);
+float soustraction(float a, float b);   
 
 int main() { 
     cout << "Programme" << endl;
-    int x_soustraction = soustraction(10, 5); 
-    int x_addition = addition(10, 5);
+    float a =100;
+    cout << "La valeur de a est : " << a << endl;
+    float x_soustraction = soustraction(10, 5); 
+    float x_addition = addition(10, 5);
+    float x = (float)addition(11, 5)/soustraction(11, 5);
 
     cout << "Le résultat est : " << x_addition << endl;
     cout << "Le résultat de la soustraction est : " << x_soustraction << endl;
+    cout << "Le résultat de la division est : " << x << endl;
     return 0;
 }
 
 // Définition de ma fonction addition
-int addition(int a, int b) {
+float addition(float a, float b) {
+    a = 20;
     return a + b;
 }
 
 // Définition de ma fonction soustraction
-int soustraction(int a, int b) {
+float soustraction(float a, float b) {
     return a - b;
 }
