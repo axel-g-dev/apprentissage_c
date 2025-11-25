7740f34a2b922bed68b44e23a777c546cba186b6
Axel <axel.gantou@carnus.fr>
2025-11-25 16:12:36 +0100

    Création de : libcercle.h cercle.h main2.c main.c

---
+++ testlib/main2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+#include "libcercle.h"
+
+int main(void) {
+    double r, p, s;
+
+    printf("Entrer le rayon du cercle : ");
+    scanf("%lf", &r);
+
+    p = peri(r);
+    s = surf(r);
+
+    printf("\nLe périmètre du cercle de rayon %lf est : %lf\n", r, p);
+    printf("La surface du cercle de rayon %lf est : %lf\n", r, s);
+
+    return 0;
+}
